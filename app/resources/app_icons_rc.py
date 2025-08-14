0e729510deb3fcf7b554f73e45a248d4a7bea12d
galdotech <mateogaldocarroo@gmail.com>
2025-08-14 04:16:50 +0200

    feat: add qss theme and menu icons

---
+++ app/resources/app_icons_rc.py
@@ -0,0 +1,181 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.9.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01X\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22><path d=\x22M17\
+ 21v-2a4 4 0 0 0\
+-4-4H5a4 4 0 0 0\
+-4 4v2\x22/><circle\
+ cx=\x229\x22 cy=\x227\x22 r\
+=\x224\x22/><path d=\x22M\
+23 21v-2a4 4 0 0\
+ 0-3-3.87\x22/><pat\
+h d=\x22M16 3.13a4 \
+4 0 0 1 0 7.75\x22/\
+></svg>\
+\x00\x00\x01@\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22><rect x=\x222\x22 \
+y=\x223\x22 width=\x2220\x22\
+ height=\x2214\x22 rx=\
+\x222\x22 ry=\x222\x22/><lin\
+e x1=\x228\x22 y1=\x2221\x22\
+ x2=\x2216\x22 y2=\x2221\x22\
+/><line x1=\x2212\x22 \
+y1=\x2217\x22 x2=\x2212\x22 \
+y2=\x2221\x22/></svg>\
+\x00\x00\x01_\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22><path d=\x22M14\
+.7 6.3a1 1 0 0 0\
+ 0 1.4l1.6 1.6a1\
+ 1 0 0 0 1.4 0l3\
+.77-3.77a6 6 0 0\
+ 1-7.94 7.94l-6.\
+91 6.91a2.12 2.1\
+2 0 0 1-3-3l6.91\
+-6.91a6 6 0 0 1 \
+7.94-7.94l-3.76 \
+3.76z\x22/></svg>\
+\x00\x00\x019\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22><path d=\x22M9 \
+21H5a2 2 0 0 1-2\
+-2V5a2 2 0 0 1 2\
+-2h4\x22/><polyline\
+ points=\x2216 17 2\
+1 12 16 7\x22/><lin\
+e x1=\x2221\x22 y1=\x2212\
+\x22 x2=\x229\x22 y2=\x2212\x22\
+/></svg>\
+\x00\x00\x01\x9c\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x2224\
+\x22 height=\x2224\x22 vi\
+ewBox=\x220 0 24 24\
+\x22 fill=\x22none\x22 st\
+roke=\x22currentCol\
+or\x22 stroke-width\
+=\x222\x22 stroke-line\
+cap=\x22round\x22 stro\
+ke-linejoin=\x22rou\
+nd\x22><path d=\x22M21\
+ 16V8a2 2 0 0 0-\
+1-1.73l-7-4a2 2 \
+0 0 0-2 0l-7 4A2\
+ 2 0 0 0 3 8v8a2\
+ 2 0 0 0 1 1.73l\
+7 4a2 2 0 0 0 2 \
+0l7-4A2 2 0 0 0 \
+21 16z\x22/><polyli\
+ne points=\x223.27 \
+6.96 12 12.01 20\
+.73 6.96\x22/><line\
+ x1=\x2212\x22 y1=\x2222.\
+08\x22 x2=\x2212\x22 y2=\x22\
+12\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x0b\
+\x05b\x05G\
+\x00c\
+\x00l\x00i\x00e\x00n\x00t\x00s\x00.\x00s\x00v\x00g\
+\x00\x0b\
+\x09\x13\xc3'\
+\x00d\
+\x00e\x00v\x00i\x00c\x00e\x00s\x00.\x00s\x00v\x00g\
+\x00\x0b\
+\x00g\xd7\xc7\
+\x00r\
+\x00e\x00p\x00a\x00i\x00r\x00s\x00.\x00s\x00v\x00g\
+\x00\x08\
+\x0f\x07WG\
+\x00e\
+\x00x\x00i\x00t\x00.\x00s\x00v\x00g\
+\x00\x0d\
+\x06%\xd0g\
+\x00i\
+\x00n\x00v\x00e\x00n\x00t\x00o\x00r\x00y\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x05\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00H\x00\x00\x00\x00\x00\x01\x00\x00\x02\xa0\
+\x00\x00\x01\x98\xa6Z\xf4\xbb\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x98\xa6Z\xf2\x0b\
+\x00\x00\x00z\x00\x00\x00\x00\x00\x01\x00\x00\x05@\
+\x00\x00\x01\x98\xa6Z\xf3\xcf\
+\x00\x00\x00,\x00\x00\x00\x00\x00\x01\x00\x00\x01\x5c\
+\x00\x00\x01\x98\xa6Z\xf2\xf7\
+\x00\x00\x00d\x00\x00\x00\x00\x00\x01\x00\x00\x04\x03\
+\x00\x00\x01\x98\xa6Z\xf5\xa3\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
